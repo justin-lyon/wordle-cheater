15b6d640d1cc49b9f3ab6645dc45911c7040c09e
Justin Lyon <jml6487@gmail.com>
2022-07-01 12:53:56 +0900

    add weighted score for dupe letters

--- src/index.js
+++ src/index.js
@@ -21,11 +21,25 @@ const has = word => yellowLetters.reduce((acc, item) => {
   return acc && word.includes(item.letter) && notHasAt(word, item)
 }, true)
 
-const score = word => {
-  const value = word.split('').reduce((acc, letter) => {
+const getScrabbleValue = word => {
+  return word.split('').reduce((acc, letter) => {
     const foundValue = scrabble.find(item => item.letters.includes(letter)).value
     return acc += foundValue
   }, 0)
+}
+
+const hasDuplicateLetter = word => {
+  return word.split('').some((letter, index, letters) => {
+    return letters.lastIndexOf(letter) !== index;
+  });
+}
+
+const score = word => {
+  const scrabbleValue = getScrabbleValue(word)
+  const hasDupe = hasDuplicateLetter(word)
+  const weightedValue = scrabbleValue * 1.5
+  const value = hasDupe ? weightedValue : scrabbleValue
+
   return {
     word,
     value
